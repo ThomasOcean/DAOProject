f05ae1330ad0688cc1e92585702bea3ebcf463aa
ThomasOcean <66387155+ThomasOcean@users.noreply.github.com>
2020-06-24 10:31:28 -0400

    Add files via upload
    Added address and department in studentDAOImp

--- StudentDAOImp.java
+++ StudentDAOImp.java
@@ -1,46 +1,49 @@
 package com.cognixia.jump.jdbc.project;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
+//import com.cognixia.jump.jdbc.project.*;
 import com.cognixia.jump.jdbc.connection.ConnectionManagerProperties;
-
+import com.cognixia.jump.jdbc.dao.Department;
+import com.cognixia.jump.jdbc.dao.DepartmentDAO;
+//import com.cognixia.jump.jdbc.dao.DepartmentDAOImp;
 public class StudentDAOImp implements StudentDAO {
 	private Connection conn = ConnectionManagerProperties.getConnection();
 	@Override
 	public List<Student> getAllStudents() {
 		List<Student> stuList = new ArrayList<>();
-		
+
 		try(Statement stmt = conn.createStatement();
 				ResultSet rs = stmt.executeQuery("select * from student"); ) {
-			
+
 			while(rs.next()) {
-				
+
 				int studentId = rs.getInt(1);
 				String firstName = rs.getString(2);
 				String lastName = rs.getString(3);
 				String gender = rs.getString(4);
-				String dateOfBirth = rs.getString(5);
+				Date dateOfBirth = rs.getDate(5);
 				int credits = rs.getInt(6);
-				int addressId = rs.getInt(7);
-				
-				Student stu = new Student (studentId, firstName, lastName, gender, dateOfBirth, credits);
+				Department dept =((DepartmentDAO)rs).getDepartmentByID(studentId);
+				Address addr = ((AddressDAO) rs).getAddressById(studentId);
+				Student stu = new Student (studentId, firstName, lastName, gender, dateOfBirth, credits,addr,dept);
 				stuList.add(stu);
-				
+
 			}
-			
-			
+
+
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 		}
-		
-		
+
+
 		return stuList;
 	}
 
@@ -48,78 +51,79 @@ public class StudentDAOImp implements StudentDAO {
 	public Student getStudentById(int id) {
 		Student stu = null;
 		try(PreparedStatement pstmt = conn.prepareStatement("select * from student where student_id = ?")) {
-			
+
 			pstmt.setInt(1, id);
-			
+
 			ResultSet rs = pstmt.executeQuery();
-			
+
 			if(rs.next()) {
-				
+
 				int studentId = rs.getInt(1);
 				String firstName = rs.getString(2);
 				String lastName = rs.getString(3);
 				String gender = rs.getString(4);
-				String dateOfBirth = rs.getString(5);
+				Date dateOfBirth = rs.getDate(5);
 				int credits = rs.getInt(6);
-				int addressId = rs.getInt(7);
+				Department dept =((DepartmentDAO)rs).getDepartmentByID(studentId);
+				Address addr = ((AddressDAO) rs).getAddressById(studentId);
 
-				
-				stu = new Student (studentId, firstName, lastName, gender, dateOfBirth, credits);
-				
+
+				stu = new Student (studentId, firstName, lastName, gender, dateOfBirth, credits,addr,dept);
+
 			}
-			
-			
+
+
 		} catch(SQLException e) {
-			
+
 		}
-		
-		
+
+
 		return stu;
 	}
 
 	@Override
 	public boolean addStudent(Student student) {
-	  Connection conn = ConnectionManagerProperties.getConnection();
+		Connection conn = ConnectionManagerProperties.getConnection();
 		// insert into student values(id, fname, lname, gender, dob, credits, addr_id, dept_id);
 		try {
 			PreparedStatement pstmt = conn.prepareStatement("insert into student values(id = ?, fname = ?, lname = ?, gender = ?, " +
 					"dob = ?, credits = ?, addr_id = ?, dept_id = ?)");
 
 			// grab the id for the department
-			int deptId = student.getDept().getId();
+			//int deptId = student.getDept().getId();
 
 			// grab the id for the address
-			int addrId = student.getAddress().getId();
+			//int addrId = student.getAddress().getId();
 
 			// grab the first name for student
-			String fname = student.getFirstName();
+			//String fname = student.getFirstName();
 
 			// grab the last name for student
-			String lname = student.getLastName();
+			//String lname = student.getLastName();
 
 			// grab the gender for student
-			String gender = student.getGender();
+			//String gender = student.getGender();
 
 			// grab the dob for student
-			int dob = student.getDob();
+			//Date dob = student.getDob();
 
 			// grab the credits for a student
-			int credits = student.getCredits();
+			//int credits = student.getCredits();
 
 			// add in your values to statement
-			pstmt.setInt(1, student.getId())
+			pstmt.setInt(1, student.getId());
 			pstmt.setInt(2, student.getDept().getId());
 			pstmt.setString(3, student.getFirstName());
 			pstmt.setString(4, student.getLastName());
 			pstmt.setString(5, student.getGender());
-			pstmt.setInt(6, student.getDob());
+			pstmt.setDate(6, student.getDob());
 			pstmt.setInt(7, student.getCredits());
 			pstmt.setInt(8, student.getAddress().getId());
 
 			int insert = pstmt.executeUpdate();
 
 			if (insert > 0) {
-				return true
+				return true;
 			}
 
 			pstmt.close();
@@ -131,19 +135,19 @@ public class StudentDAOImp implements StudentDAO {
 
 		return false;
 	}
-	
+
 	@Override
 	public boolean updateStudent(Student student) {
-		try(PreparedStatement pstmt = conn.prepareStatement("update student set first_name = ?, last_name = ?, gender = ?, date_of_birth = ?, credits = ?, where student_id = ?");) {
+		try(PreparedStatement pstmt = conn.prepareStatement("update student set first_name = ?, last_name = ?, gender = ?, date_of_birth = ?, credits = ? where student_id = ?");) {
 			
 			pstmt.setString(1, student.getFirstName());
 			pstmt.setString(2, student.getLastName());
 			pstmt.setString(3, student.getGender());
-			pstmt.setString(4, student.getDob());
+			pstmt.setDate(4, student.getDob());
 			pstmt.setInt(5, student.getCredits());
-			pstmt.setInt(6, student.getAddress().getId());
+
 			
-			pstmt.setInt(7, student.getId());
+			pstmt.setInt(6, student.getId());
 			
 			int update = pstmt.executeUpdate();
 			
@@ -155,6 +159,7 @@ public class StudentDAOImp implements StudentDAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 		return false;
 	}
 
